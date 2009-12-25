76d25052377aaa38641415ec03915b164e86bbf2
Pedro Matiello <pmatiello@gmail.com>
2009-12-25 02:15:40 -0200

    Documentation for the Link singleton.

--- src/pistache/picalculus/Link.scala
+++ src/pistache/picalculus/Link.scala
@@ -8,16 +8,29 @@ package pistache.picalculus
 
 import Name._
 
+/** An object providing methods to create new links.
+ */
 object Link {
 
+	/** Create a link of the given type.
+	 * 
+	 *  @return the name containing the link for sending and
+	 *  receiving objects from type <code>T</code>.
+     */
 	def apply[T] = new Name(new Link[T])
  
+	/** Enumeration for representing the possible actions on a
+	 *  link: sending and receiving messages.
+     */
 	object Action extends Enumeration {
 		type Action = Value
 		val Send, Receive = Value
 	}
 }
 
+/** A class representing a pi-Calculus link. Links can be used
+ *  to send names between processes.
+ */
 class Link[T] {
   
 	def ^(name:Name[T]) = new LinkProcess(this, Link.Action.Send, name)
